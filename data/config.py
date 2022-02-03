16c118852ad64a4ae400a790ac477d543e1606ed
Ergashev Baxtiyor <86454228+Baxtiyor98@users.noreply.github.com>
2022-02-03 12:15:40 +0500

    add

--- data/config.py
+++ data/config.py
@@ -7,4 +7,5 @@ env.read_env()
 # .env fayl ichidan quyidagilarni o'qiymiz
 BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
 ADMINS = env.list("ADMINS")  # adminlar ro'yxati
+GROUP = env.list("GROUP")
 IP = env.str("ip")  # Xosting ip manzili
